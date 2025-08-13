48d18a2c79c0d05c6d86e0ae9b47edf8c91e4081
SujendraNayak <sujinayak008@gmail.com>
2025-08-13 23:22:13 +0530

    `Added JSON support to Flask app, implemented add_student and Fetch_all routes, and updated database connection to include database name.`

--- New.py
+++ New.py
@@ -1,13 +1,39 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask import request
 import mysql.connector
+
 app=Flask(__name__)
 db=mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Sujendra@27",
+    password="Sujendra@27",  
     database="mydatabase"
 )
-@app.route('/addStudent',methods=['POST'])
+@app.route('/addStudent', methods=['POST'])
 def add_student():
-    data=request.
+    data = request.get_json()
+    name = data.get('name')
+    mark = data.get('mark')  
+
+    cursor = db.cursor()
+    query = "INSERT INTO students(name, mark) VALUES (%s, %s)"
+    cursor.execute(query, (name, mark))
+    db.commit()
+
+    return jsonify({'message': 'Student Added Successfully'}), 201
+
+#get method
+@app.route('/fetchall',methods=['GET'])
+def Fetch_all():
+    cursor=db.cursor(dictionary=True)
+    cursor.execute("select * from students")
+    rows=cursor.fetchall()
+    cursor.close()
+    return jsonify(rows)
+
+
+if __name__=='__main__':
+    print("connecting")
+    app.run(debug=True)
+    
+    
